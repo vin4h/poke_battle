5891da29353e36f3d67f2ae9abc0e61da3da341b
Vinicius Freitas <viniciu.freitas@gmail.com>
2022-03-02 14:22:25 -0400

    added users migration

---
+++ priv/repo/migrations/20220302181828_create_users_table.exs
@@ -0,0 +1,13 @@
+defmodule PokeBattle.Repo.Migrations.CreateUsersTable do
+  use Ecto.Migration
+
+  def change do
+   create table("users") do
+    add :name, :string
+    add :email, :string
+    add :password, :string
+
+    timestamps()
+   end
+  end
+end
