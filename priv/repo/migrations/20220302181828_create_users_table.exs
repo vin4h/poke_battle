489161d7562cb4103de2b0c2c002d70dfba288b7
Vinicius Freitas <viniciu.freitas@gmail.com>
2022-03-02 20:41:56 -0400

    add password_hash in migration users

--- priv/repo/migrations/20220302181828_create_users_table.exs
+++ priv/repo/migrations/20220302181828_create_users_table.exs
@@ -5,7 +5,7 @@ defmodule PokeBattle.Repo.Migrations.CreateUsersTable do
    create table("users") do
     add :name, :string
     add :email, :string
-    add :password, :string
+    add :password_hash, :string
 
     timestamps()
    end
